32074c9339e1a749eb2e31b4cbcef289e4210195
chenyou <chenyouz@outlook.com>
2014-09-27 22:10:22 +0800

    add passport script and login facebook module, #2

--- examples/booklog-chenyou/index.js
+++ examples/booklog-chenyou/index.js
@@ -28,22 +28,13 @@ var postSchema = new mongoose.Schema({
     content: String
 });
 
-var memberSchema = new mongoose.Schema({
-    id: {type: String, unique:true},
-    name: {type: String},
-    createDate: {type: String},
-    memberProvider:{type: String},
-    loginInfo:{}
-});
-
 //put to express framework
 //'Post'=Tablename
 //Model is definition of document.
 //new schema to a model, naming for the schema to a model named 'Post'.
 //Collection name=&model_name+"s"(lower case), e.g. posts.
 app.db = {
-  posts: mongoose.model('Post', postSchema),
-  members: mongoose.model('Member', postSchema)
+  posts: mongoose.model('Post', postSchema)
 };
 
 // Optional since express defaults to CWD/views
@@ -64,23 +55,9 @@ var posts = [{
 }];
 
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
 
 var passport = require('passport')
   , FacebookStrategy = require('passport-facebook').Strategy;
-
-app.use(passport.initialize());//use is mean middleware...(?)
-app.use(passport.session());
-
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(id, done) {
-  done(null, id);
-});
 
 passport.use(new FacebookStrategy({
     clientID: "850697018298195",
@@ -88,26 +65,10 @@ passport.use(new FacebookStrategy({
     callbackURL: "http://localhost:3000/auth/facebook/callback"
   },
   function(accessToken, refreshToken, profile, done) {
-    console.log(profile);
-    return done(null, profile);
-/*
-    var model = req.app.db.members;
-    var member = {
-       id: profile.id,
-       name: profile.displayName,
-       createDate: sysdate,
-       memberProvider:provider,
-       loginInfo:profile
-    };
-
-    var MEMBER = new model(member);//new a document by post object.
-    MEMBER.save();*/
-
-/*
-    model.findOrCreate(..., function(err, user) {
+    User.findOrCreate(..., function(err, user) {
       if (err) { return done(err); }
       done(null, user);
-    });*/
+    });
   }
 ));
 
@@ -124,6 +85,10 @@ app.get('/auth/facebook/callback',
   passport.authenticate('facebook', { successRedirect: '/',
                                       failureRedirect: '/login' }));
 
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 app.all('*', function(req, res, next){
   if (!req.get('Origin')) return next();
   // use "*" here to accept any origin
@@ -136,7 +101,6 @@ app.all('*', function(req, res, next){
 });
 
 app.get('/', function(req, res) {
-  console.log(req);
   res.render('index');
 });
 
